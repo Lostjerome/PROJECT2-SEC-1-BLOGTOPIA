afc9011cb72ead92994f2b3d3a4d849f6dffc943
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2023-03-07 23:28:44 +0700

    :lipstick: Create Members Component

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -33,12 +33,21 @@ const props = defineProps({
             <div class="grid md:grid-cols-3 gap-x-5 gap-y-8">
                 <div v-for="(person, index) in members" :key="index" 
                 class="flex flex-col md:pl-12 px-4 space-y-1">
-                    <img src="../assets/image/knuckles.jpg" class="flex object-cover h-72 w-80 rounded-xl" />
-                    <div class="text-xl font-semibold">{{ person.name }}</div> 
-                    <div class="text-zinc-700">{{ person.id }}</div>
+                    <img src="../assets/image/knuckles.jpg" 
+                        class="flex object-cover h-72 w-80 rounded-xl" />
+                    <div class="text-xl font-semibold">
+                        {{ person.name }}
+                    </div> 
+                    <div class="text-zinc-700">
+                        {{ person.id }}
+                    </div>
                     <div class="flex place-items-center gap-2">
-                        <a :href="person.githubUrl" target="_blank"><githubLogo class="text-2xl" /></a>
-                        <a :href="person.igUrl" target="_blank"><instaLogo class="text-xl" /></a>
+                        <a :href="person.githubUrl" target="_blank">
+                            <githubLogo class="text-2xl" />
+                        </a>
+                        <a :href="person.igUrl" target="_blank">
+                            <instaLogo class="text-xl" />
+                        </a>
                     </div>
 
                 </div>
