ba9d790ede2d5930113164c5479c5ab6146c33f2
darKKOREO <tanapat.daoloy@mail.kmutt.ac.th>
2023-03-09 01:02:43 +0700

    :lipstick: 1st Blog component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
+<script setup>
+import Blog from './components/Blog.vue';
+
+
+
+ 
+
+</script>
+
 <template>
   <h1 class="text-3xl font-bold underline">Blogtopia</h1>
-  <router-link to="/">Home</router-link> |
+  <router-link to="/">Home</router-link> 
   <router-link to="/about">About</router-link>
-  <router-view />
+  <Blog/>
+  <!-- <router-view /> -->
+  
 </template>
+<style>
+
+</style>
+
+
---
+++ src/components/Blog.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+const itList = ref(true)
+
+
+</script>
+<template>
+  <!-- blog -->
+  <div class="w-full h-full m-6" :class="itList == true?'flex flex-rows':'flex flex-col'">
+    <!-- image -->
+    <div class=" w-72 h-44">
+      <img src="../assets/image/skynews-drew-scanlon-blinking-white-guy_4786055.jpg" class=" rounded-2xl">
+    </div>
+  
+    <!-- content_blog -->
+        <div class=" w-96 h-32 pr-20 " :class="itList == true?'ml-3':''">
+          <!-- profile -->
+            <div class="flex flex-rows">
+                <!-- profile_pic -->
+                <div class=" bg-slate-400 w-7 h-7 rounded-full"></div>
+                <!-- username&date_to_post -->
+                <h2 class=" ml-1">Tanapat&nbsp;Daoloy</h2>
+                <h2 class=" text-slate-300 ">&nbsp;·&nbsp;Month&nbsp;dd,yyyy</h2>
+            </div>
+           <!-- preview_content --> 
+          <div>
+            <h1 class=" font-bold mt-2 mb-2 text-2xl">Blog's title</h1>
+            <p class=" break-words text-slate-400">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+          </div>
+        </div>
+  </div>
+
+
+</template>
