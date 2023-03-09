511d4da3c17d8c8057f067808cf24a57766d015d
CRYINGV <60339342+CRYINGV@users.noreply.github.com>
2023-03-09 20:41:13 +0700

    eiei

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <a href="/" class="flex">
           <span class="self-center text-3xl font-bold whitespace-nowrap dark:text-white">Blogtopia</span>
         </a>
-        <div id="docsearch" class="ml-10">
+        <div class="ml-10">
         <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500  hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200  rounded-lg text-sm p-2.5 mr-1" >
       <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
       <span class="sr-only">Search</span>
