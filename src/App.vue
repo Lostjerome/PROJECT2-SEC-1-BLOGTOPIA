ad2398b023255e540db61e2a4bdb020e732a5794
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2023-03-09 20:31:25 +0700

    :lipstick: Update Members Component

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,9 @@
   <router-link to="/">Home</router-link> |
   <router-link to="/about">About</router-link>
   <router-view />
+  <Member />
 </template>
+
+<script setup>
+  import Member from './components/Members.vue'
+</script>
