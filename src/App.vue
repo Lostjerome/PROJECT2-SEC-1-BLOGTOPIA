917dbe0546c8e4c58f873d6933fbc57c3028bbc7
CRYINGV <60339342+CRYINGV@users.noreply.github.com>
2023-03-09 20:40:13 +0700

    EueuEiei

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,41 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Blogtopia</h1>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
-  <router-view />
+  
+  <div class="flex items-center justify-between w-full px-3 py-3 mx-auto max-w-8xl lg:px-4 mt-8 mb-2">
+    
+      <div class="flex items-center justify-between ml-40">
+        <a href="/" class="flex">
+          <span class="self-center text-3xl font-bold whitespace-nowrap dark:text-white">Blogtopia</span>
+        </a>
+        <div id="docsearch" class="ml-10">
+        <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500  hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200  rounded-lg text-sm p-2.5 mr-1" >
+      <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+      <span class="sr-only">Search</span>
+    </button>
+    <div class="relative hidden md:block">
+      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        <span class="sr-only">Search icon</span>
+      </div>
+      <input type="text" id="search-navbar" class="block w-full p-2 pl-10 pr-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50" placeholder="What do you want to read ?">
+    </div>
+  </div>
+      </div>
+      
+     
+
+    <div class="flex items-center flex-none mr-40">
+      <ul class="flex-col pt-6 lg:flex-row lg:self-center lg:py-0 lg:flex ">
+        <li class="mb-3 lg:px-2 xl:px-2 lg:mb-0">
+          <a href="" class="text-l font-bold text-gray-900 hover:text-blue-600">Meet our team</a>
+        </li>
+        <li class=" mb-3 lg:px-2 xl:px-2 lg:mb-0 xl:block">
+          <a href="" class="text-l font-bold text-gray-900 hover:text-blue-600">Sign in</a>
+        </li>
+      </ul>
+    
+        <a href="" class="xl:inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-full text-sm px-5 py-2.5 text-center ml-5">Get Started</a>
+      </div>
+  </div>
+  <hr>
+
 </template>
