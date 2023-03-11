814648c4a2430ac9e759541c5dde26d0d2e4d34b
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2023-03-11 21:11:28 +0700

    :lipstick: Update Members Component

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -1,61 +1,45 @@
 <script setup>
-import { ref }  from 'vue'
-import getMember from '../composable/getMember'
 import githubLogo from './icons/SkillIconsGithubLight.vue'
 import instaLogo from './icons/LogosInstagramIcon.vue'
 
-const members = ref(getMember())
-
 const props = defineProps({
-    name : {
-        type : String,
-        required : true
-    },
-    studentId : {
-        type : String,
-        required : true
-    },
-    githubUrl : {
-        type : String
-    },
-    instagramUrl : {
-        type : String
-    },
-    image : {
-        type : String
+        name : {
+            type : String,
+            required : true
+        },
+        studentId : {
+            type : String,
+            required : true
+        },
+        githubUrl : {
+            type : String
+        },
+        instagramUrl : {
+            type : String
+        },
+        image : {
+            type : String
+        }
     }
-}
 )
-
 </script>
  
 <template>
-    <div class="flex flex-col max-w-7xl min-w-fit m-auto">
-        <div class="text-3xl  font-bold m-8 my-4">
-            Meet the team
-        </div>
-            <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-y-8 justify-items-center">
-                <div v-for="(person, index) in members" :key="index" 
-                class="flex flex-col mx-10 space-y-1">
-                    <img src="../assets/image/knuckles.jpg" 
-                        class="flex object-cover h-72 w-80 rounded-xl" />
-                    <div class="text-xl font-semibold">
-                        {{ person.name }}
-                    </div> 
-                    <div class="text-zinc-700">
-                        {{ person.id }}
-                    </div>
-                    <div class="flex place-items-center gap-2">
-                        <a :href="person.githubUrl" target="_blank">
-                            <githubLogo class="text-2xl" />
-                        </a>
-                        <a :href="person.igUrl" target="_blank">
-                            <instaLogo class="text-xl" />
-                        </a>
-                    </div>
-
-                </div>
-            </div>
+    <img :src="image" 
+        class="flex object-cover h-72 w-80 rounded-xl" />
+    <div class="text-xl font-semibold">
+        {{ name }}
+    </div> 
+    <div class="text-zinc-700">
+        {{ studentId }}
+    </div>
+    <div class="flex place-items-center gap-2">
+        <a :href="githubUrl" target="_blank">
+            <githubLogo class="text-2xl" />
+        </a>
+        <a :href="instagramUrl" target="_blank">
+            <instaLogo class="text-xl" />
+        </a>
     </div>
 </template>
  
--- src/views/OurTeam.vue
+++ src/views/OurTeam.vue
@@ -1,8 +1,28 @@
 <script setup>
+import { ref } from "vue";
 import Member from "../components/Members.vue";
+import getMember from "../composable/getMember";
+
+const members = ref(getMember())
+console.log(getMember())
+
+
 </script>
 <template>
-  <div>
-    <Member />
+  <div class="flex flex-col max-w-7xl min-w-fit m-auto">
+    <div class="text-3xl  font-bold ml-10 ">
+      Meet the team
+    </div>
+      <div class="flex flex-wrap justify-center gap-y-8 my-10">
+        <div v-for="(person, index) in members" :key="index" 
+          class="flex flex-col mx-10 space-y-1">
+            <Member 
+              :name="person.name" 
+              :student-id="person.id"
+              :github-url="person.githubUrl"
+              :instagram-url="person.igUrl"
+              :image="person.img" />
+    </div>
+  </div>
   </div>
 </template>
