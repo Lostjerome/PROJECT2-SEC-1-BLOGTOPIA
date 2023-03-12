6a51722766b59673827ebd66e8e28fed6d269104
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2023-03-12 22:48:40 +0700

    :lipstack Update some UI

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -10,7 +10,7 @@ const props = defineProps({
         studentId : {
             type : String,
             required : true
-        },
+        },  
         githubUrl : {
             type : String
         },
@@ -25,21 +25,24 @@ const props = defineProps({
 </script>
  
 <template>
-    <img :src="image" 
-        class="flex object-cover h-72 w-80 rounded-xl" />
-    <div class="text-xl font-semibold">
-        {{ name }}
-    </div> 
-    <div class="text-zinc-700">
-        {{ studentId }}
-    </div>
-    <div class="flex place-items-center gap-2">
-        <a :href="githubUrl" target="_blank">
-            <githubLogo class="text-2xl" />
-        </a>
-        <a :href="instagramUrl" target="_blank">
-            <instaLogo class="text-xl" />
-        </a>
+    <div class="space-y-1">
+        <img :src="image" 
+            class="flex object-cover h-72 w-80 rounded-lg" />
+        <div class="text-base font-semibold uppercase">
+            {{ name }}
+        </div> 
+        <div class="text-sm font-semibold text-[#000000b9] border-b-2">
+            {{ studentId }}
+        </div>
+        <div class="flex place-items-center gap-2">
+            <a :href="githubUrl" target="_blank">
+                <githubLogo class="text-2xl" />
+            </a>
+            <a :href="instagramUrl" target="_blank">
+                <instaLogo class="text-xl" />
+            </a>
+
+        </div>
     </div>
 </template>
  
