c0ed579bdb44aa115eba63442debc1e9df3a8a81
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2023-03-27 14:53:57 +0700

    :hammer: add create member

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -3,29 +3,51 @@ import githubLogo from './icons/SkillIconsGithubLight.vue'
 import instaLogo from './icons/LogosInstagramIcon.vue'
 
 const props = defineProps({
-        name : {
-            type : String,
+        // name : {
+        //     type : String,
+        //     required : true
+        // },
+        // studentId : {
+        //     type : String,
+        //     required : true
+        // },  
+        // githubUrl : {
+        //     type : String
+        // },
+        // instagramUrl : {
+        //     type : String
+        // },
+        // image : {
+        //     type : String
+        // }
+        member : {
+            type : Object,
             required : true
-        },
-        studentId : {
-            type : String,
-            required : true
-        },  
-        githubUrl : {
-            type : String
-        },
-        instagramUrl : {
-            type : String
-        },
-        image : {
-            type : String
         }
     }
 )
+
 </script>
  
 <template>
-        <img :src="image" 
+        <img :src="member.img" 
+            class="flex object-cover h-64 w-72 rounded-lg" />
+        <div class="text-base font-semibold uppercase pt-2">
+            {{ member.name }}
+        </div> 
+        <div class="text-sm font-semibold text-[#000000b9] pb-1 border-b-2">
+            {{ member.id }}
+        </div>
+        <div class="flex place-items-center gap-2">
+            <a :href="member.url.github" target="_blank">
+                <githubLogo class="text-2xl" />
+            </a>
+            <a :href="member.url.ig" target="_blank">
+                <instaLogo class="text-xl" />
+            </a>
+        </div>
+
+        <!-- <img :src="image" 
             class="flex object-cover h-64 w-72 rounded-lg" />
         <div class="text-base font-semibold uppercase pt-2">
             {{ name }}
@@ -40,7 +62,8 @@ const props = defineProps({
             <a :href="instagramUrl" target="_blank">
                 <instaLogo class="text-xl" />
             </a>
-        </div>
+        </div> -->
+
 
 </template>
  
