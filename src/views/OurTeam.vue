d650f6df26cad0e66ff1237918e2b7c4d32d55ab
darKKOREO <tanapat.daoloy@mail.kmutt.ac.th>
2023-03-30 02:57:55 +0700

    :triangular_flag_on_post: 1st edit-member

--- src/views/OurTeam.vue
+++ src/views/OurTeam.vue
@@ -2,96 +2,112 @@
 import { onMounted, ref, computed } from "vue";
 import Member from "../components/Members.vue";
 import getMember from "../composable/getMember";
-import AddMemberIcon from '../components/icons/MaterialSymbolsPersonAddRounded.vue'
-import { deleteMembers } from '../composable/deleteMembers.js';
+import AddMemberIcon from "../components/icons/MaterialSymbolsPersonAddRounded.vue";
+import { deleteMembers } from "../composable/deleteMembers.js";
 
-const members = ref([])
-const member = ref({})
+const itsEdit = ref(false)
+const members = ref([]);
+const member = ref({});
 
 onMounted(async () => {
   members.value = await getMember();
-});
-
-onMounted(() => {
   member.value = {
     id: "",
     name: "",
     github: "",
     ig: "",
-    img: ""
-  }
-})
+    img: "",
+  };
+});
 
 // Btn
-const setAddMember = ref(false)
+const setAddMember = ref(false);
 const toggleAddMember = () => {
-  setAddMember.value = !setAddMember.value
-  selectedBinaryFile.value = ""
-  previewSrc.value = ""
-  member.value = {}
-}
+  setAddMember.value = !setAddMember.value;
+  selectedBinaryFile.value = "";
+  previewSrc.value = "";
+  member.value = {};
+};
 
-// Add member
+// Add member 
 const addNewMember = async () => {
-  member.value.img = previewSrc.value
-  console.log(member.value.name)
+  member.value.img = previewSrc.value;
+  console.log(member.value.name);
+  const objectMember = {
+    id: member.value.id,
+    name: member.value.name,
+    url: {
+      github: member.value.github,
+      ig: member.value.ig,
+    },
+    img: member.value.img,
+  };
   if (!isItComplete()) return;
-  (isItComplete())
-  try{
-    const response = await fetch('http://localhost:5000/members',{
-      method : 'POST',
-			headers : {
-				'Content-Type': 'application/json'
-			},
-      body : JSON.stringify({
-        id : member.value.id,
-        name : member.value.name,
-        url : {
-          github : member.value.github,
-          ig : member.value.ig
-        },
-        img: member.value.img
-      })
-    })
-    console.log('add successfully')
-    const addedMember = await response.json()	// add backend เสร็จ ถึงไป add ใน frontend
-    members.value.push(addedMember)
-    console.log(members.value)
-    toggleAddMember()
-  }
-  catch (error) {
-    console.log(error.m)
-  }
-}
+  isItComplete();
 
-// Select image 
-const selectedBinaryFile = ref("")
-const previewSrc = ref("")
+  try {
+    const urlPath = itsEdit.value === true ? `http://localhost:5000/members/${member.value.id}` : "http://localhost:5000/members"
+    const response = await fetch(urlPath, {
+      method: itsEdit.value === true ? "PUT" :"POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objectMember),
+    });
+    console.log("add successfully");
+    // const addedMember = await response.json(); // add backend เสร็จ ถึงไป add ใน frontend
+    // members.value.push(addedMember);
+    members.value = await getMember();
+    console.log(members.value);
+    toggleAddMember();
+  } catch (error) {
+    console.log(error.m);
+  }
+};
+
+// Select image
+const selectedBinaryFile = ref("");
+const previewSrc = ref("");
 
 const chooseBinaryFile = (e) => {
-  (selectedBinaryFile.value = e.target.files[0])
-  console.log(selectedBinaryFile.value)
-}
+  selectedBinaryFile.value = e.target.files[0];
+  console.log(selectedBinaryFile.value);
+};
 
 const canPreview = computed(() => {
-  if (typeof selectedBinaryFile.value === 'object') {
-    previewImage()
-    console.log(previewSrc.value)
-    return true
+  if (previewSrc.value !== "") {
+    return true;
+  } else if (typeof selectedBinaryFile.value === "object") {
+    previewImage();
+    console.log(previewSrc.value);
+    return true;
   }
-  return false
-})
+  return false;
+});
 
 const previewImage = () => {
   const reader = new FileReader();
-  reader.readAsDataURL(selectedBinaryFile.value)
+  reader.readAsDataURL(selectedBinaryFile.value);
   reader.onload = () => {
     previewSrc.value = reader.result;
-  }
+  };
   reader.onerror = (error) => {
     console.log("Error: ", error);
-  }
-}
+  };
+};
+
+// getData-member from id
+const editMember = async (id) => {
+  itsEdit.value = true
+  setAddMember.value = !setAddMember.value;
+  const getDataMember = await getMember(id);
+  console.log(getDataMember.img);
+
+  previewSrc.value = getDataMember.img;
+  member.value = getDataMember;
+  member.value.github = getDataMember.url.github;
+  member.value.ig = getDataMember.url.ig;
+};
 
 // Delete member
 const checkBeforeDelete = (id) => {
@@ -100,113 +116,137 @@ const checkBeforeDelete = (id) => {
     deleteMembers(id);
     members.value = members.value.filter((member) => member.id !== id);
   }
-}; 
+};
 
 // Check form input
 const isItComplete = () => {
-  if(member.value.name === undefined){
-    alert("Please enter your name")
-    return false
+  if (member.value.name === undefined) {
+    alert("Please enter your name");
+    return false;
+  } else if (member.value.img === "") {
+    alert("Please choose your image");
+    return false;
   }
-  else if(member.value.img === "") {
-    alert("Please choose your image")
-    return false
-  }
-  return true
-}
-
+  return true;
+};
 </script>
 <template>
   <!-- Form Add Member -->
-  <div v-show="setAddMember" 
-  class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-20 flex place-content-center overflow-auto p-4">
-    <div class="border rounded-3xl shadow-2xl px-16 pt-14 bg-white w-max m-auto ">
-      <!-- Image -->
-      <div class=" h-64 w-72 bg-slate-200 hover:bg-slate-300 duration-200 rounded-xl">
-        <label for="file-upload" >
-          <div class=" h-64 w-72 bg-slate-200 hover:bg-slate-300 duration-200 rounded-xl">
-            <img 
-              :src="previewSrc" 
-              v-show="canPreview" 
-              alt="preview" class="object-cover h-64 w-72 rounded-lg m-auto" 
-            />
-          </div>    
-        </label>
-      </div>
-      <input 
-        id="file-upload" 
-        type="file" 
-        accept=".png,.jpg,.jpeg" 
-        @change="chooseBinaryFile" 
-        class="hidden"   
-      />
-      <!-- Information -->
+  <div>
+    <div
+      v-show="setAddMember"
+      class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-20 flex place-content-center overflow-auto p-4"
+    >
+      <div
+        class="border rounded-3xl shadow-2xl px-16 pt-14 bg-white w-max m-auto"
+      >
+        <!-- Image -->
+        <div
+          class="h-64 w-72 bg-slate-200 hover:bg-slate-300 duration-200 rounded-xl"
+        >
+          <label for="file-upload">
+            <div
+              class="h-64 w-72 bg-slate-200 hover:bg-slate-300 duration-200 rounded-xl"
+            >
+              <img
+                :src="previewSrc"
+                v-show="canPreview"
+                alt="preview"
+                class="object-cover h-64 w-72 rounded-lg m-auto"
+              />
+            </div>
+          </label>
+        </div>
+        <input
+          id="file-upload"
+          type="file"
+          accept=".png,.jpg,.jpeg"
+          @change="chooseBinaryFile"
+          class="hidden"
+        />
+        <!-- Information -->
         <div class="text-base font-semibold pt-2">
-          Student Id : <br>
-          <input 
-          type="text" 
-          placeholder="64xxxxxxxxx"
-          v-model="member.id" 
-          class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic" 
+          Student Id : <br />
+          <input
+            type="text"
+            placeholder="64xxxxxxxxx"
+            v-model="member.id"
+            class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic"
           />
         </div>
         <div class="text-base font-semibold pt-2">
-          Name : <br>
-          <input 
-          type="text" 
-          placeholder="Harry Potter" 
-          v-model="member.name"
-          class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic"
+          Name : <br />
+          <input
+            type="text"
+            placeholder="Harry Potter"
+            v-model="member.name"
+            class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic"
           />
         </div>
         <div class="text-base font-semibold pt-2">
-          Github Url : <br>
-          <input 
-          type="text" 
-          placeholder="https://github.com/username" 
-          v-model="member.github" 
-          class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic" 
+          Github Url : <br />
+          <input
+            type="text"
+            placeholder="https://github.com/username"
+            v-model="member.github"
+            class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic"
           />
         </div>
         <div class="text-base font-semibold pt-2">
-          Instagrm Url : <br>
-          <input 
-          type="text" 
-          placeholder="https://www.instagram.com/username/" 
-          v-model="member.ig" 
-          class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic"
+          Instagrm Url : <br />
+          <input
+            type="text"
+            placeholder="https://www.instagram.com/username/"
+            v-model="member.ig"
+            class="border rounded-md px-3 w-full placeholder:text-xs placeholder:italic"
           />
         </div>
         <!-- Button -->
         <div class="my-5 flex justify-between">
-          <button 
-          class="py-1 px-5 text-sm font-semibold bg-blue-700 hover:bg-blue-800 text-white hover: rounded-full"
-          @click="addNewMember()">            
-          Add
+          <button
+            class="py-1 px-5 text-sm font-semibold bg-blue-700 hover:bg-blue-800 text-white hover: rounded-full"
+            @click="addNewMember()"
+          >
+            Add
           </button>
-          <button 
-          class="py-1 px-3 text-sm font-semibold border bg-[#fff] hover:bg-gray-200 text-black rounded-full"
-          @click="toggleAddMember()">            
-          Cancel
+          <button
+            class="py-1 px-3 text-sm font-semibold border bg-[#fff] hover:bg-gray-200 text-black rounded-full"
+            @click="toggleAddMember()"
+          >
+            Cancel
           </button>
-        </div> 
-    </div>
-  </div>
-
-  <!-- Meet our Team -->
-  <div>
-    <div class="flex max-w-6xl m-auto">
-      <div>
-        <div class="flex justify-between">
-          <div class="text-3xl font-bold ml-10 mt-6">We can't do this without</div>
         </div>
-        <div class="flex flex-wrap justify-center gap-y-8 my-10">
-          <div v-for="(person, index) in members" :key="index" class="flex flex-col mx-10 space-y-1">
-            <Member :member="person" @delete="checkBeforeDelete"/>
+      </div>
+    </div>
+
+    <!-- Meet our Team -->
+    <div>
+      <div class="flex max-w-6xl m-auto">
+        <div>
+          <div class="flex justify-between">
+            <div class="text-3xl font-bold ml-10 mt-6">
+              We can't do this without
+            </div>
           </div>
-          <div @click="toggleAddMember"
-            class="box-content h-64 w-72 border border-3 mx-10 space-y-1 rounded-lg grid place-content-center ">
-            <AddMemberIcon class="text-5xl" />
+          <div class="flex flex-wrap justify-center gap-y-8 my-10">
+            <div
+              v-for="(person, index) in members"
+              :key="index"
+              class="flex flex-col mx-10 space-y-1"
+            >
+              <!-- emit here -->
+              <Member
+                :member="person"
+                @edit="editMember"
+                @delete="checkBeforeDelete"
+              />
+            </div>
+            <div
+              @click="toggleAddMember"
+              class="box-content h-64 w-72 border border-3 mx-10 space-y-1 rounded-lg grid place-content-center cursor-pointer"
+            >
+              <AddMemberIcon class="text-5xl" />
+            </div>
           </div>
         </div>
       </div>
